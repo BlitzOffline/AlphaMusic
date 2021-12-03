c6b823371a641855164cf64eafb074ebdfca6dca
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2021-12-03 18:28:06 +0200

    Initial jda stuff

--- src/main/kotlin/com/blitzoffline/alphamusic/AlphaMusic.kt
+++ src/main/kotlin/com/blitzoffline/alphamusic/AlphaMusic.kt
@@ -1,7 +1,13 @@
 package com.blitzoffline.alphamusic
 
 import com.blitzoffline.alphamusic.settings.SettingsHandler
+import com.blitzoffline.alphamusic.settings.holders.Bot
+import dev.triumphteam.cmd.slash.SlashCommandManager
 import me.mattstudios.config.SettingsManager
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.requests.GatewayIntent
+import net.dv8tion.jda.api.utils.cache.CacheFlag
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -11,9 +17,29 @@ class AlphaMusic {
     lateinit var settings: SettingsManager
         private set
 
+    lateinit var jda: JDA
+        private set
 
     fun run() {
         val settingsHandler = SettingsHandler(this)
         settings = settingsHandler.fetchSettings()
+
+        jda = createJDAInstance().awaitReady()
+
+        val slashCommandManager = SlashCommandManager.createDefault(jda)
     }
+
+    private fun createJDAInstance() = JDABuilder
+        .create(
+            settings[Bot.TOKEN],
+            listOf(
+                GatewayIntent.GUILD_VOICE_STATES
+            )
+        )
+        .disableCache(
+            CacheFlag.ACTIVITY,
+            CacheFlag.CLIENT_STATUS,
+            CacheFlag.ONLINE_STATUS
+        )
+        .build()
 }
