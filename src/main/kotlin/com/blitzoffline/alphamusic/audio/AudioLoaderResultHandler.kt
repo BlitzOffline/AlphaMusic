1ce5158d788fbc3f898d97fa1f80b35294e1f75a
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2021-12-03 22:48:01 +0200

    bumped cli commons version, fixed audio loader result handler

--- src/main/kotlin/com/blitzoffline/alphamusic/audio/AudioLoaderResultHandler.kt
+++ src/main/kotlin/com/blitzoffline/alphamusic/audio/AudioLoaderResultHandler.kt
@@ -7,20 +7,23 @@ import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 
-// todo: Fix track loaded method not being called!
 class AudioLoaderResultHandler(private val event: SlashCommandEvent, private val musicManager: GuildMusicManager, private val deferred: Boolean = false) : AudioLoadResultHandler {
     override fun trackLoaded(track: AudioTrack) {
         track.userData = TrackMetadata(event.user)
         if (musicManager.audioHandler.queue(track)) {
-            event.terminate("Added song to queue.", deferred)
+            event.terminate("Added song to queue.", deferred = deferred)
         } else {
-            event.terminate("Queue is full. Could not add song.", deferred)
+            event.terminate("Queue is full. Could not add song.", deferred = deferred)
         }
     }
 
     override fun playlistLoaded(playlist: AudioPlaylist) {
         if (musicManager.audioHandler.queue.remainingCapacity() == 0) {
-            return event.terminate("Queue is full. Could not add any more songs.", deferred)
+            return event.terminate("Queue is full. Could not add any more songs.", deferred = deferred)
+        }
+
+        if (playlist.tracks.isEmpty()) {
+            return event.terminate("Could not find any songs!", deferred = deferred)
         }
 
         var count = 0
@@ -32,23 +35,23 @@ class AudioLoaderResultHandler(private val event: SlashCommandEvent, private val
         }
 
         if (count == 1) {
-            event.terminate("Added $count song to the queue.", deferred)
+            event.terminate("Added $count song to the queue.", deferred = deferred)
         } else {
-            event.terminate("Added $count songs to the queue.", deferred)
+            event.terminate("Added $count songs to the queue.", deferred = deferred)
         }
     }
 
     override fun noMatches() {
-        event.terminate("Could not find any songs!", deferred)
+        event.terminate("Could not find any songs!", deferred = deferred)
     }
 
     override fun loadFailed(throwable: FriendlyException) {
         val message = throwable.message
 
         if (throwable.severity == FriendlyException.Severity.COMMON && message != null) {
-            event.terminate(message, deferred)
+            event.terminate(message, deferred = deferred)
         } else {
-            event.terminate("Something went wrong while loading!", deferred)
+            event.terminate("Something went wrong while loading!", deferred = deferred)
         }
     }
 }
