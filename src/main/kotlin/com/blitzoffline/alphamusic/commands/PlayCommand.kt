ce014a2fe5a1abad713ae396b5d42095d3069a82
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2021-12-05 13:38:42 +0200

    Updated the play command to not have any sub commands and only have one option

--- src/main/kotlin/com/blitzoffline/alphamusic/commands/PlayCommand.kt
+++ src/main/kotlin/com/blitzoffline/alphamusic/commands/PlayCommand.kt
@@ -5,6 +5,7 @@ import com.blitzoffline.alphamusic.audio.AudioLoaderResultHandler
 import com.blitzoffline.alphamusic.utils.process
 import dev.triumphteam.cmd.core.BaseCommand
 import dev.triumphteam.cmd.core.annotation.Command
+import dev.triumphteam.cmd.core.annotation.Default
 import dev.triumphteam.cmd.core.annotation.Description
 import dev.triumphteam.cmd.core.annotation.SubCommand
 import dev.triumphteam.cmd.slash.sender.SlashSender
@@ -12,59 +13,18 @@ import dev.triumphteam.cmd.slash.sender.SlashSender
 @Command("play")
 @Description("Play a song!")
 class PlayCommand(private val bot: AlphaMusic) : BaseCommand() {
-    @SubCommand("link")
-    @Description("Find a song using a link!")
-    fun SlashSender.link(link: String) {
+    private val URL_REGEX = "^(https?)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]".toRegex()
+
+    @Default
+    fun SlashSender.play(song: String) {
         event.deferReply().queue()
         if (!process(join = true, sameChannel = true, adminBypass = true, deferred = true)) {
             return
         }
 
         val guild = guild ?: return
-
         val musicManager = bot.getGuildMusicManager(guild)
-        bot.playerManager.loadItemOrdered(musicManager.player, link, AudioLoaderResultHandler(event, musicManager, true))
-    }
-
-    @SubCommand("youtube")
-    @Description("Find a song on youtube using keywords!")
-    fun SlashSender.youtube(keywords: String) {
-        event.deferReply().queue()
-        if (!process(join = true, sameChannel = true, adminBypass = true, deferred = true)) {
-            return
-        }
-
-        val guild = guild ?: return
-
-        val musicManager = bot.getGuildMusicManager(guild)
-        bot.playerManager.loadItemOrdered(musicManager.player, "ytsearch:$keywords", AudioLoaderResultHandler(event, musicManager, true))
-    }
-
-    @SubCommand("youtube-music")
-    @Description("Find a song on youtube music using keywords!")
-    fun SlashSender.youtubeMusic(keywords: String) {
-        event.deferReply().queue()
-        if (!process(join = true, sameChannel = true, adminBypass = true, deferred = true)) {
-            return
-        }
-
-        val guild = guild ?: return
-
-        val musicManager = bot.getGuildMusicManager(guild)
-        bot.playerManager.loadItemOrdered(musicManager.player, "ytmsearch:$keywords", AudioLoaderResultHandler(event, musicManager, true))
-    }
-
-    @SubCommand("soundcloud")
-    @Description("Find a song on soundcloud using keywords!")
-    fun SlashSender.soundcloud(keywords: String) {
-        event.deferReply().queue()
-        if (!process(join = true, sameChannel = true, adminBypass = true, deferred = true)) {
-            return
-        }
-
-        val guild = guild ?: return
-
-        val musicManager = bot.getGuildMusicManager(guild)
-        bot.playerManager.loadItemOrdered(musicManager.player, "scsearch:$keywords", AudioLoaderResultHandler(event, musicManager, true))
+        val track = if (URL_REGEX.matches(song)) song else "ytsearch:${song}"
+        bot.playerManager.loadItemOrdered(musicManager.player, track, AudioLoaderResultHandler(event, musicManager, true))
     }
 }
