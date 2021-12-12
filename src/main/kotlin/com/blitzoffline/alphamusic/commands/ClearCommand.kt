855a2e0009f96474444fe8666c55f5b6919c0f25
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2021-12-12 19:38:13 +0200

    Made the clear commit use requirements and also the new voting system

--- src/main/kotlin/com/blitzoffline/alphamusic/commands/ClearCommand.kt
+++ src/main/kotlin/com/blitzoffline/alphamusic/commands/ClearCommand.kt
@@ -1,31 +1,64 @@
 package com.blitzoffline.alphamusic.commands
 
 import com.blitzoffline.alphamusic.AlphaMusic
-import com.blitzoffline.alphamusic.utils.process
 import com.blitzoffline.alphamusic.utils.terminate
+import com.blitzoffline.alphamusic.votes.VoteType
 import dev.triumphteam.cmd.core.BaseCommand
 import dev.triumphteam.cmd.core.annotation.Command
 import dev.triumphteam.cmd.core.annotation.Default
 import dev.triumphteam.cmd.core.annotation.Description
+import dev.triumphteam.cmd.core.annotation.Requirement
+import dev.triumphteam.cmd.core.annotation.Requirements
 import dev.triumphteam.cmd.slash.sender.SlashSender
+import kotlin.math.ceil
+import net.dv8tion.jda.api.Permission
 
 @Command("clear")
 @Description("Clear the queue!")
 class ClearCommand(private val bot: AlphaMusic) : BaseCommand() {
     @Default
+    @Requirements(
+        Requirement("IN_GUILD", messageKey = "IN_GUILD"),
+        Requirement("BOT_IS_IN_VC", messageKey = "BOT_IS_IN_VC"),
+        Requirement("SAME_CHANNEL_OR_ADMIN", messageKey = "SAME_CHANNEL_OR_ADMIN")
+    )
     fun SlashSender.clear() {
-        if (!process(sameChannel = true, adminBypass = true)) {
-            return
-        }
-
         val guild = guild ?: return
-        val musicManager = bot.getMusicManager(guild)
+        val member = member ?: return
 
+        val musicManager = bot.getMusicManager(guild)
         if (musicManager.audioHandler.queue.isEmpty()) {
             return event.terminate("The queue is already empty!")
         }
 
-        musicManager.audioHandler.queue.clear()
-        event.terminate("Cleared the queue!")
+        if (member.permissions.contains(Permission.ADMINISTRATOR)) {
+            musicManager.audioHandler.queue.clear()
+            return event.terminate("Cleared the queue!")
+        }
+
+        val participants = guild.selfMember.voiceState?.channel?.members ?: return
+
+        if (participants.size <= 2) {
+            musicManager.audioHandler.queue.clear()
+            return event.terminate("Cleared the queue!")
+        }
+
+        val voteManager = musicManager.votesManager.getVoteManager(VoteType.CLEAR)
+            ?: return event.terminate("Could not process your vote!")
+
+        if (voteManager.votes.contains(member.id)) {
+            return event.terminate("You have already voted for the queue to be cleared!")
+        }
+
+        val required = if (participants.size <= 4) 2 else ceil(participants.size * 0.5).toInt()
+
+        if (voteManager.votes.size + 1 == required) {
+            voteManager.votes.clear()
+            musicManager.audioHandler.queue.clear()
+            return event.terminate("Cleared the queue!")
+        }
+
+        voteManager.votes.add(member.id)
+        return event.terminate("Added vote for queue clear. Total votes: ${voteManager.votes.size}/$required!")
     }
 }
--- src/main/kotlin/com/blitzoffline/alphamusic/listeners/BotReadyListener.kt
+++ src/main/kotlin/com/blitzoffline/alphamusic/listeners/BotReadyListener.kt
@@ -23,6 +23,10 @@ import com.blitzoffline.alphamusic.commands.StopCommand
 import com.blitzoffline.alphamusic.commands.VolumeCommand
 import com.github.ygimenez.model.PaginatorBuilder
 import com.github.ygimenez.type.Emote
+import dev.triumphteam.cmd.core.message.MessageKey
+import dev.triumphteam.cmd.core.message.context.MessageContext
+import dev.triumphteam.cmd.core.requirement.RequirementKey
+import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.events.ReadyEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
@@ -34,6 +38,36 @@ class BotReadyListener(private val bot: AlphaMusic) : ListenerAdapter() {
             .shouldEventLock(true)
             .setEmote(Emote.CANCEL, "✖️")
             .activate()
+
+        bot.manager.registerRequirement(RequirementKey.of("IN_GUILD")) { sender ->
+            sender.guild != null && sender.member != null
+        }
+
+        bot.manager.registerRequirement(RequirementKey.of("BOT_IS_IN_VC")) { sender ->
+            sender.guild?.selfMember?.voiceState?.channel != null
+        }
+
+        bot.manager.registerRequirement(RequirementKey.of("SAME_CHANNEL_OR_ADMIN")) { sender ->
+            val member = sender.member
+
+            if (member == null) {
+                false
+            } else {
+                member.permissions.contains(Permission.ADMINISTRATOR) || member.voiceState?.channel == sender.guild?.selfMember?.voiceState?.channel
+            }
+        }
+
+        bot.manager.registerMessage(MessageKey.of("IN_GUILD", MessageContext::class.java)) { sender, _ ->
+            sender.reply("This command can only be used in a guild!").queue()
+        }
+
+        bot.manager.registerMessage(MessageKey.of("BOT_IS_IN_VC", MessageContext::class.java)) { sender, _ ->
+            sender.reply("The bot is currently not connected to a voice channel!").queue()
+        }
+
+        bot.manager.registerMessage(MessageKey.of("SAME_CHANNEL_OR_ADMIN", MessageContext::class.java)) { sender, _ ->
+            sender.reply("You need to be in the same Voice Channel as the bot to do this!").queue()
+        }
 
         val guild = bot.jda.guilds.first() ?: return bot.logger.warn("Couldn't find any guilds!")
         bot.manager.registerCommand(
