bd05b145678231ddc38ced576e02dd24b9d044c7
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2021-12-27 19:20:19 +0200

    Switched the /np command to using requirements

--- src/main/kotlin/com/blitzoffline/alphamusic/commands/NowPlayingCommand.kt
+++ src/main/kotlin/com/blitzoffline/alphamusic/commands/NowPlayingCommand.kt
@@ -2,27 +2,29 @@ package com.blitzoffline.alphamusic.commands
 
 import com.blitzoffline.alphamusic.AlphaMusic
 import com.blitzoffline.alphamusic.utils.asEmbed
-import com.blitzoffline.alphamusic.utils.process
 import com.blitzoffline.alphamusic.utils.terminate
 import dev.triumphteam.cmd.core.BaseCommand
 import dev.triumphteam.cmd.core.annotation.Command
 import dev.triumphteam.cmd.core.annotation.Default
 import dev.triumphteam.cmd.core.annotation.Description
+import dev.triumphteam.cmd.core.annotation.Requirement
+import dev.triumphteam.cmd.core.annotation.Requirements
 import dev.triumphteam.cmd.slash.sender.SlashSender
 
 @Command("np")
 @Description("List currently playing song!")
 class NowPlayingCommand(private val bot: AlphaMusic) : BaseCommand() {
     @Default
+    @Requirements(
+        Requirement("IN_GUILD", messageKey = "IN_GUILD"),
+        Requirement("BOT_IS_IN_VC", messageKey = "BOT_IS_IN_VC"),
+    )
     fun SlashSender.nowPlaying() {
         deferReply().queue()
-        if (!process(deferred = true)) {
-            return
-        }
-
         val guild = guild ?: return
         val musicManager = bot.getMusicManager(guild)
-        val playing = musicManager.player.playingTrack.asEmbed(guild.selfMember.avatarUrl) ?: return event.terminate("There is no song playing currently!", deferred = true)
+        val playing = musicManager.player.playingTrack.asEmbed(guild.selfMember.avatarUrl)
+            ?: return event.terminate("There is no song playing currently!", deferred = true)
 
         event.terminate(playing, deferred = true)
     }
