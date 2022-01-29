1497ea3447e72de2b95f65d49e569e4b8b296c22
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2022-01-29 21:17:45 +0200

    A user's votes are removed when they leave

--- src/main/kotlin/com/blitzoffline/alphamusic/listeners/VoiceChannelListener.kt
+++ src/main/kotlin/com/blitzoffline/alphamusic/listeners/VoiceChannelListener.kt
@@ -1,6 +1,7 @@
 package com.blitzoffline.alphamusic.listeners
 
 import com.blitzoffline.alphamusic.AlphaMusic
+import com.blitzoffline.alphamusic.votes.VoteType
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
@@ -8,13 +9,17 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter
 class VoiceChannelListener(private val bot: AlphaMusic) : ListenerAdapter() {
     override fun onGuildVoiceLeave(event: GuildVoiceLeaveEvent) {
         val guild = event.guild
+        val musicManager = bot.getMusicManager(guild)
         if (event.member == guild.selfMember) {
-            val musicManager = bot.getMusicManager(guild)
             musicManager.player.isPaused = true
 
             bot.tasksManager.removeLeaveTask(guild.id)
             bot.tasksManager.addClearTask(musicManager)
+            musicManager.votesManager.clear()
         } else {
+            VoteType.values.forEach { voteType ->
+                musicManager.votesManager.getVoteManager(voteType)?.votes?.remove(event.member.id)
+            }
             if (event.channelLeft != guild.selfMember.voiceState?.channel) return
             if (event.channelLeft.members.size >= 2) return
             if (event.channelLeft.members[0] != guild.selfMember) return
