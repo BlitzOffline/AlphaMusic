2ba0dfc7683c6fe3bf84aa152570361433c7e781
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2022-02-04 22:14:00 +0200

    Added console command to stop the bot

--- src/main/kotlin/com/blitzoffline/alphamusic/Application.kt
+++ src/main/kotlin/com/blitzoffline/alphamusic/Application.kt
@@ -1,9 +1,11 @@
 package com.blitzoffline.alphamusic
 
+import com.blitzoffline.alphamusic.console.ConsoleCommands
 import com.blitzoffline.alphamusic.utils.CommandLine
 
 fun main(args: Array<String>) {
     val cli = CommandLine(args)
     val bot = AlphaMusic(cli.fetchTokenFromFlag(), cli.fetchEmailFromFlag(), cli.fetchPassFromFlag())
     bot.run()
+    ConsoleCommands(bot).run()
 }
