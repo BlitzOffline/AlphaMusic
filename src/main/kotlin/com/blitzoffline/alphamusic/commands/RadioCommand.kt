8e497159e27922d695665abf90d42f5df6328c23
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2022-02-10 10:50:13 +0200

    Added voting for the radio command

--- src/main/kotlin/com/blitzoffline/alphamusic/commands/RadioCommand.kt
+++ src/main/kotlin/com/blitzoffline/alphamusic/commands/RadioCommand.kt
@@ -2,6 +2,7 @@ package com.blitzoffline.alphamusic.commands
 
 import com.blitzoffline.alphamusic.AlphaMusic
 import com.blitzoffline.alphamusic.utils.terminate
+import com.blitzoffline.alphamusic.votes.VoteType
 import dev.triumphteam.cmd.core.BaseCommand
 import dev.triumphteam.cmd.core.annotation.Command
 import dev.triumphteam.cmd.core.annotation.Default
@@ -25,7 +26,10 @@ class RadioCommand(private val bot: AlphaMusic) : BaseCommand() {
         val member = member ?: return
         val musicManager = bot.getMusicManager(guild)
 
+        val voteManager = musicManager.voteHandler.getVoteManager(VoteType.RADIO)
+
         if (member.permissions.contains(Permission.ADMINISTRATOR)) {
+            voteManager?.votes?.clear()
             musicManager.audioHandler.radio = !musicManager.audioHandler.radio
             return if (musicManager.audioHandler.radio) {
                 event.terminate("Radio mode has been enabled!")
@@ -34,11 +38,35 @@ class RadioCommand(private val bot: AlphaMusic) : BaseCommand() {
             }
         }
 
-        musicManager.audioHandler.radio = !musicManager.audioHandler.radio
-        return if (musicManager.audioHandler.radio) {
-            event.terminate("Radio mode has been enabled!")
-        } else {
-            event.terminate("Radio mode has been disabled!")
+        val participants = guild.selfMember.voiceState?.channel?.members ?: return
+
+        if (participants.size <= 2) {
+            voteManager?.votes?.clear()
+            musicManager.audioHandler.queue.clear()
+            return event.terminate("Cleared the queue!")
         }
+
+        if (voteManager == null) {
+            return event.terminate("Could not process your vote!")
+        }
+
+        if (voteManager.votes.contains(member.id)) {
+            return event.terminate("You have already voted!")
+        }
+
+        val required = voteManager.getRequiredVotes(participants.size)
+
+        if (voteManager.votes.size + 1 == required) {
+            voteManager.votes.clear()
+            musicManager.audioHandler.radio = !musicManager.audioHandler.radio
+            return if (musicManager.audioHandler.radio) {
+                event.terminate("Radio mode has been enabled!")
+            } else {
+                event.terminate("Radio mode has been disabled!")
+            }
+        }
+
+        voteManager.votes.add(member.id)
+        return event.terminate("Added vote for the radio to be toggled. Total votes: ${voteManager.votes.size}/$required!")
     }
 }
