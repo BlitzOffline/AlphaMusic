8a7a7478f8630f145901a7ec184dbfb745cda285
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2022-02-10 11:28:07 +0200

    Added voting system to the /shuffle command

--- src/main/kotlin/com/blitzoffline/alphamusic/commands/ShuffleCommand.kt
+++ src/main/kotlin/com/blitzoffline/alphamusic/commands/ShuffleCommand.kt
@@ -2,6 +2,7 @@ package com.blitzoffline.alphamusic.commands
 
 import com.blitzoffline.alphamusic.AlphaMusic
 import com.blitzoffline.alphamusic.utils.terminate
+import com.blitzoffline.alphamusic.votes.VoteType
 import dev.triumphteam.cmd.core.BaseCommand
 import dev.triumphteam.cmd.core.annotation.Command
 import dev.triumphteam.cmd.core.annotation.Default
@@ -9,6 +10,7 @@ import dev.triumphteam.cmd.core.annotation.Description
 import dev.triumphteam.cmd.core.annotation.Requirement
 import dev.triumphteam.cmd.core.annotation.Requirements
 import dev.triumphteam.cmd.slash.sender.SlashSender
+import net.dv8tion.jda.api.Permission
 
 @Command("shuffle")
 @Description("Shuffle the queue!")
@@ -21,14 +23,47 @@ class ShuffleCommand(private val bot: AlphaMusic) : BaseCommand() {
     )
     fun SlashSender.shuffle() {
         val guild = guild ?: return
+        val member = member ?: return
         val musicManager = bot.getMusicManager(guild)
+        val voteManager = musicManager.voteHandler.getVoteManager(VoteType.SHUFFLE)
 
         when (musicManager.audioHandler.size()) {
-            0 -> event.terminate("There are no songs in the queue to be shuffled.")
-            1 -> event.terminate("There is only one song in the queue.")
-            else -> musicManager.audioHandler.shuffle()
-        }
+            0 -> return event.terminate("There are no songs in the queue to be shuffled.")
+            1 -> return event.terminate("There is only one song in the queue.")
+            else -> {
+                if (member.permissions.contains(Permission.ADMINISTRATOR)) {
+                    voteManager?.votes?.clear()
+                    musicManager.audioHandler.shuffle()
+                    return event.terminate("The queue was successfully shuffled.")
+                }
 
-        event.terminate("The queue was successfully shuffled.")
+                val participants = guild.selfMember.voiceState?.channel?.members ?: return
+
+                if (participants.size <= 2) {
+                    voteManager?.votes?.clear()
+                    musicManager.audioHandler.shuffle()
+                    return event.terminate("The queue was successfully shuffled.")
+                }
+
+                if (voteManager == null) {
+                    return event.terminate("Could not process your vote!")
+                }
+
+                if (voteManager.votes.contains(member.id)) {
+                    return event.terminate("You have already voted!")
+                }
+
+                val required = voteManager.getRequiredVotes(participants.size)
+
+                if (voteManager.votes.size + 1 == required) {
+                    voteManager.votes.clear()
+                    musicManager.audioHandler.shuffle()
+                    return event.terminate("The queue was successfully shuffled.")
+                }
+
+                voteManager.votes.add(member.id)
+                return event.terminate("Added vote for the queue to be shuffled. Total votes: ${voteManager.votes.size}/$required!")
+            }
+        }
     }
 }
