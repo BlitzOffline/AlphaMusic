c00b94e1ff997e7999300564025e4b275fd6bece
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2022-02-10 11:38:18 +0200

    Added a voting system for the /stop command

--- src/main/kotlin/com/blitzoffline/alphamusic/commands/StopCommand.kt
+++ src/main/kotlin/com/blitzoffline/alphamusic/commands/StopCommand.kt
@@ -2,6 +2,7 @@ package com.blitzoffline.alphamusic.commands
 
 import com.blitzoffline.alphamusic.AlphaMusic
 import com.blitzoffline.alphamusic.utils.terminate
+import com.blitzoffline.alphamusic.votes.VoteType
 import dev.triumphteam.cmd.core.BaseCommand
 import dev.triumphteam.cmd.core.annotation.Command
 import dev.triumphteam.cmd.core.annotation.Default
@@ -9,6 +10,7 @@ import dev.triumphteam.cmd.core.annotation.Description
 import dev.triumphteam.cmd.core.annotation.Requirement
 import dev.triumphteam.cmd.core.annotation.Requirements
 import dev.triumphteam.cmd.slash.sender.SlashSender
+import net.dv8tion.jda.api.Permission
 
 @Command("stop")
 @Description("Stop the audio!")
@@ -20,19 +22,53 @@ class StopCommand(private val bot: AlphaMusic) : BaseCommand() {
         Requirement("same_channel_or_admin", messageKey = "not_same_channel_or_admin"),
     )
     fun SlashSender.stop() {
-        // todo: Make the bot also leave on stop.
         val guild = guild ?: return
-
+        val member = member ?: return
         val musicManager = bot.getMusicManager(guild)
 
         if (musicManager.player.playingTrack == null && musicManager.audioHandler.size() == 0) {
             return event.terminate("The bot is not playing any audio!")
         }
 
-        musicManager.audioHandler.clear()
-        musicManager.player.stopTrack()
-        bot.taskManager.addLeaveTask(guild)
+        val voteManager = musicManager.voteHandler.getVoteManager(VoteType.CLEAR)
 
-        event.terminate("Stopped the audio!")
+        if (member.permissions.contains(Permission.ADMINISTRATOR)) {
+            voteManager?.votes?.clear()
+            musicManager.audioHandler.clear()
+            musicManager.player.stopTrack()
+            bot.taskManager.addLeaveTask(guild)
+            return event.terminate("Stopped the audio!")
+        }
+
+        val participants = guild.selfMember.voiceState?.channel?.members ?: return
+
+        if (participants.size <= 2) {
+            voteManager?.votes?.clear()
+            musicManager.audioHandler.clear()
+            musicManager.player.stopTrack()
+            bot.taskManager.addLeaveTask(guild)
+            return event.terminate("Stopped the audio!")
+        }
+
+        if (voteManager == null) {
+            return event.terminate("Could not process your vote!")
+        }
+
+        if (voteManager.votes.contains(member.id)) {
+            return event.terminate("You have already voted!")
+        }
+
+        val required = voteManager.getRequiredVotes(participants.size)
+
+        if (voteManager.votes.size + 1 == required) {
+            voteManager?.votes?.clear()
+            musicManager.audioHandler.clear()
+            musicManager.player.stopTrack()
+            bot.taskManager.addLeaveTask(guild)
+            return event.terminate("Stopped the audio!")
+        }
+
+        voteManager.votes.add(member.id)
+        return event.terminate("Added vote for the bot to be stopped. Total votes: ${voteManager.votes.size}/$required!")
     }
 }
