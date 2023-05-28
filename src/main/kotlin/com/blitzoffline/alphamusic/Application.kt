bdcca12c6716e9d0d63983af7666410f390630a7
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2023-05-28 22:08:41 +0300

    Improved startup & console commands.

--- src/main/kotlin/com/blitzoffline/alphamusic/Application.kt
+++ src/main/kotlin/com/blitzoffline/alphamusic/Application.kt
@@ -1,16 +1,34 @@
 package com.blitzoffline.alphamusic
 
-import com.blitzoffline.alphamusic.console.ConsoleCommands
-import com.blitzoffline.alphamusic.utils.StartupParametersParser
+import com.blitzoffline.alphamusic.console.ConsoleApplication
+import com.blitzoffline.alphamusic.console.commands.StartupCommand
+import com.blitzoffline.alphamusic.console.commands.handleException
+import org.apache.commons.cli.HelpFormatter
+import org.apache.commons.cli.ParseException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.slf4j.event.Level
+import kotlin.system.exitProcess
+
+private val logger: Logger = LoggerFactory.getLogger(AlphaMusic::class.java)
 
 fun main(args: Array<String>) {
-    val startupParametersParser = StartupParametersParser(args)
-    val bot = AlphaMusic(
-        startupParametersParser.parseDiscordBotToken(),
-        startupParametersParser.parseYoutubeEmail(),
-        startupParametersParser.parseYoutubePassword()
-    )
+    val helpFormatter = HelpFormatter()
+    var startupCommand = StartupCommand()
 
-    bot.run()
-    ConsoleCommands(bot).run()
+    try {
+        startupCommand = startupCommand.parse(args)
+    } catch (exception: ParseException) {
+        val commandLine = args.joinToString(" ")
+        if (!handleException(exception, commandLine, startupCommand.options, logger, Level.ERROR)) {
+            logger.error("Something went wrong while starting the bot.", exception)
+            logger.info("Something went wrong while executing command: $commandLine", exception)
+        }
+
+        helpFormatter.printHelp("AlphaMusic.jar", startupCommand.options, true)
+        exitProcess(0)
+    }
+
+    val bot = AlphaMusic(logger, startupCommand.getDiscordToken(), startupCommand.getYoutubeEmail(), startupCommand.getYoutubePassword()).run()
+    val console = ConsoleApplication(bot.jda, logger).run()
 }
---
+++ src/main/kotlin/com/blitzoffline/alphamusic/utils/Logger.kt
@@ -0,0 +1,24 @@
+package com.blitzoffline.alphamusic.utils
+
+import org.slf4j.Logger
+import org.slf4j.event.Level
+
+fun Logger.log(level: Level, message: String) {
+    when (level) {
+        Level.TRACE -> trace(message)
+        Level.DEBUG -> debug(message)
+        Level.INFO -> info(message)
+        Level.WARN -> warn(message)
+        Level.ERROR -> error(message)
+    }
+}
+
+fun Logger.log(level: Level, message: String, throwable: Throwable) {
+    when (level) {
+        Level.TRACE -> trace(message, throwable)
+        Level.DEBUG -> debug(message, throwable)
+        Level.INFO -> info(message, throwable)
+        Level.WARN -> warn(message, throwable)
+        Level.ERROR -> error(message, throwable)
+    }
+}
