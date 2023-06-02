2b6aed48a9ffd679c81ca7c96c0262fa4e3dc57a
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2023-06-02 13:14:59 +0300

    used proper constructor

--- src/main/kotlin/com/blitzoffline/alphamusic/utils/EnvironmentVariables.kt
+++ src/main/kotlin/com/blitzoffline/alphamusic/utils/EnvironmentVariables.kt
@@ -15,27 +15,25 @@ data class EnvironmentVariables(
     val databaseSaveDelay: Long,
     val databaseSaveDelayTimeUnit: TimeUnit
 ) {
-    companion object {
-        operator fun invoke(
-            discordToken: String? = null,
-            youtubeEmail: String? = null,
-            youtubePassword: String? = null,
-            databaseHost: String? = null,
-            databaseName: String? = null,
-            databaseUsername: String? = null,
-            databasePassword: String? = null,
-            databaseSaveDelay: Long? = null,
-            databaseSaveDelayTimeUnit: TimeUnit? = null
-        ) = EnvironmentVariables(
-            discordToken = discordToken ?: System.getenv("ALPHAMUSIC_DISCORD_TOKEN") ?: System.getProperty("ALPHAMUSIC_DISCORD_TOKEN") ?: throw MissingArgumentException("Could not find environment variable ALPHAMUSIC_DISCORD_TOKEN"),
-            youtubeEmail = youtubeEmail ?: System.getenv("ALPHAMUSIC_YOUTUBE_EMAIL") ?: System.getProperty("ALPHAMUSIC_YOUTUBE_EMAIL"),
-            youtubePassword = youtubePassword ?: System.getenv("ALPHAMUSIC_YOUTUBE_PASSWORD") ?: System.getProperty("ALPHAMUSIC_YOUTUBE_PASSWORD"),
-            databaseHost = databaseHost ?: System.getenv("ALPHAMUSIC_MYSQL_HOST") ?: System.getProperty("ALPHAMUSIC_MYSQL_HOST") ?: throw MissingArgumentException("Could not find environment variable ALPHAMUSIC_MYSQL_HOST"),
-            databaseName = databaseName ?: System.getenv("ALPHAMUSIC_MYSQL_DATABASE") ?: System.getProperty("ALPHAMUSIC_MYSQL_DATABASE") ?: throw MissingArgumentException("Could not find environment variable ALPHAMUSIC_MYSQL_DATABASE"),
-            databaseUsername = databaseUsername ?: System.getenv("ALPHAMUSIC_MYSQL_USERNAME") ?: System.getProperty("ALPHAMUSIC_MYSQL_USERNAME") ?: throw MissingArgumentException("Could not find environment variable ALPHAMUSIC_MYSQL_USERNAME"),
-            databasePassword = databasePassword ?: System.getenv("ALPHAMUSIC_MYSQL_PASSWORD") ?: System.getProperty("ALPHAMUSIC_MYSQL_PASSWORD") ?: throw MissingArgumentException("Could not find environment variable ALPHAMUSIC_MYSQL_PASSWORD"),
-            databaseSaveDelay = databaseSaveDelay ?: System.getenv("ALPHAMUSIC_MYSQL_DELAY")?.toLong() ?: System.getProperty("ALPHAMUSIC_MYSQL_DELAY")?.toLong() ?: 30,
-            databaseSaveDelayTimeUnit = databaseSaveDelayTimeUnit ?: (System.getenv("ALPHAMUSIC_MYSQL_DELAY_TIMEUNIT") ?: System.getProperty("ALPHAMUSIC_MYSQL_DELAY_TIMEUNIT")).toTimeUnit()
-        )
-    }
+    constructor(
+        discordToken: String? = null,
+        youtubeEmail: String? = null,
+        youtubePassword: String? = null,
+        databaseHost: String? = null,
+        databaseName: String? = null,
+        databaseUsername: String? = null,
+        databasePassword: String? = null,
+        databaseSaveDelay: Long? = null,
+        databaseSaveDelayTimeUnit: TimeUnit? = null
+    ) : this(
+        discordToken = discordToken ?: System.getenv("ALPHAMUSIC_DISCORD_TOKEN") ?: System.getProperty("ALPHAMUSIC_DISCORD_TOKEN") ?: throw MissingArgumentException("Could not find environment variable ALPHAMUSIC_DISCORD_TOKEN"),
+        youtubeEmail = youtubeEmail ?: System.getenv("ALPHAMUSIC_YOUTUBE_EMAIL") ?: System.getProperty("ALPHAMUSIC_YOUTUBE_EMAIL"),
+        youtubePassword = youtubePassword ?: System.getenv("ALPHAMUSIC_YOUTUBE_PASSWORD") ?: System.getProperty("ALPHAMUSIC_YOUTUBE_PASSWORD"),
+        databaseHost = databaseHost ?: System.getenv("ALPHAMUSIC_MYSQL_HOST") ?: System.getProperty("ALPHAMUSIC_MYSQL_HOST") ?: throw MissingArgumentException("Could not find environment variable ALPHAMUSIC_MYSQL_HOST"),
+        databaseName = databaseName ?: System.getenv("ALPHAMUSIC_MYSQL_DATABASE") ?: System.getProperty("ALPHAMUSIC_MYSQL_DATABASE") ?: throw MissingArgumentException("Could not find environment variable ALPHAMUSIC_MYSQL_DATABASE"),
+        databaseUsername = databaseUsername ?: System.getenv("ALPHAMUSIC_MYSQL_USERNAME") ?: System.getProperty("ALPHAMUSIC_MYSQL_USERNAME") ?: throw MissingArgumentException("Could not find environment variable ALPHAMUSIC_MYSQL_USERNAME"),
+        databasePassword = databasePassword ?: System.getenv("ALPHAMUSIC_MYSQL_PASSWORD") ?: System.getProperty("ALPHAMUSIC_MYSQL_PASSWORD") ?: throw MissingArgumentException("Could not find environment variable ALPHAMUSIC_MYSQL_PASSWORD"),
+        databaseSaveDelay = databaseSaveDelay ?: System.getenv("ALPHAMUSIC_MYSQL_DELAY")?.toLong() ?: System.getProperty("ALPHAMUSIC_MYSQL_DELAY")?.toLong() ?: 30,
+        databaseSaveDelayTimeUnit = databaseSaveDelayTimeUnit ?: (System.getenv("ALPHAMUSIC_MYSQL_DELAY_TIMEUNIT") ?: System.getProperty("ALPHAMUSIC_MYSQL_DELAY_TIMEUNIT")).toTimeUnit()
+    )
 }
