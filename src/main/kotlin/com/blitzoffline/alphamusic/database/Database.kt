4e3759ca46c66dd875662ec7d75daf8769020cb2
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2023-06-02 13:22:15 +0300

    Added optional env variable to specify host port

--- src/main/kotlin/com/blitzoffline/alphamusic/database/Database.kt
+++ src/main/kotlin/com/blitzoffline/alphamusic/database/Database.kt
@@ -7,7 +7,7 @@ import org.jetbrains.exposed.sql.Database
 
 class Database(private val environmentVariables: EnvironmentVariables) {
     private val config = HikariConfig().apply {
-        this.jdbcUrl = "jdbc:mysql://${environmentVariables.databaseHost}/${environmentVariables.databaseName}?useSsl=false&serverTimezone=UTC"
+        this.jdbcUrl = "jdbc:mysql://${environmentVariables.databaseHost}:${environmentVariables.databasePort}/${environmentVariables.databaseName}?useSsl=false&serverTimezone=UTC"
         this.driverClassName = "com.mysql.cj.jdbc.Driver"
         this.username = environmentVariables.databaseUsername
         this.password = environmentVariables.databasePassword
